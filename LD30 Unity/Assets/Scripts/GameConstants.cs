10e94a71ea9b13f655f33bffade036d7b874d044
fbrizu <frank.brizuela@mail.mcgill.ca>
2014-08-24 04:39:26 -0400

    people moving

--- LD30 Unity/Assets/Scripts/GameConstants.cs
+++ LD30 Unity/Assets/Scripts/GameConstants.cs
@@ -10,7 +10,7 @@ public class GameConstants : MonoBehaviour {
 	public static int peopleLayerMask = 1 << 10;
 
 	//Island
-	public static float islandMaxSpeed = 0.5f;
+	public static float islandMaxSpeed = 1f;
 	public static float islandDepth = 10f;
 	public static int numIslands = 3; //Number of island sprites in the game
 
--- LD30 Unity/Assets/Scripts/PeopleController.cs
+++ LD30 Unity/Assets/Scripts/PeopleController.cs
@@ -4,7 +4,10 @@ using System.Collections;
 public class PeopleController : PersonController {
 
 	// Use this for initialization
-	void Start () {
+	protected override void Start () {
+		selected = false;
+		connected = false;
+
 		RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(0f, 0f), 0.1f, GameConstants.islandLayerMask);
 
 		if(hit.collider != null) {
@@ -16,9 +19,18 @@ public class PeopleController : PersonController {
 			Destroy(gameObject);
 		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	protected override void Update() {
+		base.Update();
+
+		//New update
+		if(!connected) {
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(0f, 0f), 0.1f, GameConstants.islandLayerMask);
+			if(hit.collider != null) {
+				if(hit.collider.tag == "Island") {
+					connected = hit.collider.GetComponent<IslandController>().connected;
+				}
+			}
+		}
 	}
 }
